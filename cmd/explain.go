201d9bda315fac3bb215bab9e958926cfca174d8
Kwame Owusu <alfred.owusu@zing.dev>
2025-09-05 15:31:14 +0100

    make lookup in upper case

--- cmd/explain.go
+++ cmd/explain.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/kwame-Owusu/sidl/internal"
 	"github.com/spf13/cobra"
@@ -17,17 +18,25 @@ var explainCmd = &cobra.Command{
 
 func explainSid(cmd *cobra.Command, args []string) {
 	sid := args[0]
-	isValidSid, err := internal.IsValidSid(sid, sids)
+	upperSid := strings.ToUpper(sid)
+	prefix := strings.ToUpper(sid[0:2]) // Make sure prefix is uppercase too
+
+	isValidSid, err := internal.IsValidSid(upperSid, sids)
 	if err != nil {
-		fmt.Printf("Error occured while checking if sid is valid, %s\n", err)
-	}
-	if !isValidSid {
-		argIdentifier := args[0][0:2]
-		fmt.Printf("Unknown SID prefix: %s\n", argIdentifier)
+		fmt.Printf("Error occurred while checking if sid is valid: %s\n", err)
+		return
 	}
 
-	if description, ok := sids[sid[0:2]]; ok {
+	if !isValidSid {
+		fmt.Printf("Unknown SID prefix: %s\n", prefix)
+		return
+	}
+
+	// Use the uppercase prefix to lookup in the map
+	if description, ok := sids[prefix]; ok {
 		fmt.Printf("Name: %s\n", description.Name)
 		fmt.Printf("Description: %s\n", description.Description)
+	} else {
+		fmt.Printf("SID prefix %s not found in sids.json\n", prefix)
 	}
 }
