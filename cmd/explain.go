caabd738e14da08503fcf11d4b3e892a62befb2f
Kwame Owusu <alfred.owusu@zing.dev>
2025-09-05 12:38:06 +0100

    added explain command

--- cmd/explain.go
+++ cmd/explain.go
@@ -1,1 +1,31 @@
 package cmd
+
+import (
+	"fmt"
+
+	"github.com/kwame-Owusu/sidl/internal"
+	"github.com/spf13/cobra"
+)
+
+var explainCmd = &cobra.Command{
+	Use:   "explain [description]",
+	Short: "Get info about a sid",
+	Long:  "Get the detailed information about a SID",
+	Args:  cobra.MinimumNArgs(1),
+	Run:   explainSid,
+}
+
+func explainSid(cmd *cobra.Command, args []string) {
+	sid := args[0]
+	isValidSid, err := internal.IsValidSid(sid, sids)
+	if err != nil {
+		fmt.Printf("Error occured while checking if sid is valid: %s", err)
+	}
+	if !isValidSid {
+		fmt.Printf("Not a valid SID, please refer to twilio docs or check sidly list, %s", args[0])
+	}
+
+	if description, ok := sids[sid]; ok {
+		fmt.Printf("description: %s", description.Description)
+	}
+}
--- cmd/root.go
+++ cmd/root.go
@@ -30,6 +30,7 @@ var rootCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(listCmd)
+	rootCmd.AddCommand(explainCmd)
 	loadSids()
 }
 
